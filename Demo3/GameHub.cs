82add5c99622ce25512c11648bfb93d14f24035e
OdedCohenGadol <odedushka@gmail.com>
2015-09-10 19:49:43 +0300

    first

---
+++ Demo3/GameHub.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Demo3.Models;
+
+namespace Demo3
+{
+    public class GameHub : Hub
+    {
+        public bool join(string username)
+        {
+            var player = GameState.Instance.GetPlayer(username);
+            if (player != null)
+            {
+                Clients.Caller.playerExists();
+                return true;
+            }
+
+            //player is new' add him
+            player = GameState.Instance.CreatePlayer(username);
+            player.ConnectionId = Context.ConnectionId;
+            Clients.Caller.name = player.Name;
+            Clients.Caller.hash = player.Hash;
+            Clients.Caller.id = player.Id;
+
+            Clients.Caller.playerJoined(player);
+
+            return StartGame(player);
+        }
+
+        private bool StartGame(Player player)
+        {
+            if (player != null)
+            {
+                Player player2;
+                var game = GameState.Instance.FindGame(player, out player2);
+                if (game != null)
+                {
+                    Clients.Group(player.Group).buildBoard(game);
+                    return true;
+                }
+
+                //no game founded -- create new
+                player2 = GameState.Instance.GetNewOpponent(player);
+                if (player2 == null)
+                {
+                    //no one looking for play, sit and wait
+                    Clients.Caller.waitingList();
+                    return true;
+                }
+
+                game = GameState.Instance.CreateGame(player, player2);
+                game.WhosTurn = player.Id;
+
+                Clients.Group(player.Group).buildBoard(game);
+                return true;
+            }
+            return false;
+        }
+
+        public bool Flip(string cardName)
+        {
+            var username = Clients.Caller.name;
+            var player = GameState.Instance.GetPlayer(username);
+            if (player != null)
+            {
+                Player playerOpponent;
+                var game = GameState.Instance.FindGame(player, out playerOpponent);
+                if (game != null)
+                {
+                    if (!string.IsNullOrEmpty(game.WhosTurn) && game.WhosTurn != player.id )
+                    {
+                        return true;
+                    }
+
+                    var card = FindCard(game, cardName);
+                    Clients.Group(player.group).flipCard(card);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private Card FindCard(Game game, string cardName)
+        {
+            return game.Board.Pieces.FirstOrDefault(p => p.Name == cardName);
+        }
+
+        public bool CheckCard(string cardName)
+        {
+            var username = Clients.Caller.name;
+            Player player = GameState.Instance.GetPlayer(username);
+            if (player != null)
+            {
+                Player playerOpponent;
+                var game = GameState.Instance.FindGame(player, out playerOpponent);
+                if (game != null)
+                {
+                    if (!string.IsNullOrEmpty(game.WhosTurn) && game.WhosTurn != player.Id)
+                    {
+                        return true;
+                    }
+                    Card card = FindCard(game, cardName);
+                    if (game.LastCard == null)
+                    {
+                        game.WhosTurn = player.Id;
+                        game.LastCard = card;
+                        return true;
+                    }
+
+                    //second flip
+                    bool isMatch = IsMatch(game, card);
+                    if (isMatch)
+                    {
+                        StoreMatch(player, card);
+                        game.LastCard = null;
+                        Clients.Group(player.Group).showMatch(card, username);
+
+                        if (player.Matches.Count >= 16)
+                        {
+                            Clients.Group(player.Group).winner(card, username);
+                            GameState.Instance.ResetGame(game);
+                            return true;
+                        }
+                        return true;
+                    }
+
+                    Player opponent = GameState.Instance.GetOpponent(player,game);
+                    //shift turn
+                    game.WhosTurn = opponent.Id;
+
+                    Clients.Group(player.Group).resetFlip(game.LastCard, card);
+                    game.LastCard = null;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void StoreMatch(Player player, Card card)
+        {
+            player.Matches.Add(card.Id);
+            player.Matches.Add(card.Pair);
+        }
+
+        private bool IsMatch(Game game, Card card)
+        {
+            if (card == null)
+            {
+                return false;
+            }
+            if (game.LastCard != null)
+            {
+                //two cards have picked, lets check them
+                if (game.LastCard.Pair == card.Id)
+                {
+                    //we have a match
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+    }
+}
---
+++ Demo3/GameState.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Web;
+using Demo3.Models;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Demo3
+{
+    public class GameState
+    {
+        private static readonly Lazy<GameState> _instance = new Lazy<GameState>(
+            () => new GameState(GlobalHost.ConnectionManager.GetHubContext<GameHub>()));
+
+        private readonly ConcurrentDictionary<string, Player> _players = 
+            new ConcurrentDictionary<string, Player>(StringComparer.OrdinalIgnoreCase);
+
+        private readonly ConcurrentDictionary<string, Game> _games =
+            new ConcurrentDictionary<string, Game>(StringComparer.OrdinalIgnoreCase);
+
+        public GameState(IHubContext context)
+        {
+            Clients = context.Clients;
+            Groups = context.Groups;
+        }
+
+        public static GameState Instance
+        {
+            get { return _instance.Value; }
+        }
+
+        public IHubConnectionContext Clients {get;set;} 
+        public IGroupManager Groups { get; set; }
+
+        public Player CreatePlayer(string username)
+        {
+            var player = new Player(username, GetMD5Hash(username));
+            _players[username] = player;
+            return player;
+        }
+
+        private string GetMD5Hash(string username)
+        {
+            return String.Join("", MD5.Create()
+                        .ComputeHash(Encoding.Default.GetBytes(username))
+                        .Select(b => b.ToString("x2")));
+        }
+
+
+        public Player GetPlayer(string username)
+        {
+            return _players.Values.FirstOrDefault(p => p.Name == username);
+        }
+
+        public Player GetNewOpponent(Player player)
+        {
+            return _players.Values.FirstOrDefault(p=> !p.IsPlaying && p.Id != player.Id);
+        }
+
+        public Player GetOpponent(Player player, Game game)
+        {
+            if (game.Player1.Id == player.Id)
+            {
+                return game.Player2;
+            }
+            else
+            {
+                return game.Player1;
+            }
+        }
+
+        public Game CreateGame(Player player1, Player player2)
+        {
+            var game = new Game
+            {
+                Player1 = player1,
+                Player2 = player2,
+                Board = new Board()
+            };
+
+            var group = Guid.NewGuid().ToString("d");
+            _games[group] = game;
+
+            player1.IsPlaying = player2.IsPlaying = true;
+            player1.Group = player2.Group = group;
+
+            Groups.Add(player1.ConnectionId, group);
+            Groups.Add(player2.ConnectionId, group);
+
+
+            return game;
+        }
+
+        public Game FindGame(Player player, out Player opponent)
+        {
+            opponent = null;
+            if (player.Group == null)
+            {
+                return null;
+            }
+            Game game;
+            _games.TryGetValue(player.Group, out game);
+            if (game != null)
+            {
+                if (player.Id == game.Player1.Id)
+                {
+                    opponent = game.Player2;
+                    return game;
+                }
+                opponent = game.Player1;
+                return game;
+            }
+            return null;
+        }
+
+        public void ResetGame(Game game)
+        {
+            var groupName = game.Player1.Group;
+            var player1Name = game.Player1.Name;
+            var player2Name = game.Player2.Name;
+
+            Groups.Remove(game.Player1.ConnectionId, groupName);
+            Groups.Remove(game.Player2.ConnectionId, groupName);
+
+            Player p1;
+            _players.TryRemove(player1Name, out p1);
+
+            Player p2;
+            _players.TryRemove(player2Name, out p2);
+
+            Game g;
+            _games.TryRemove(groupName, out g);
+        }
+    }
+}
